b696fb19455a62be1a650e34861e76c7044cd4a3
mikicola <michela.colangelo@gmail.com>
2022-03-16 11:23:53 +0100

    inizio a definire script

---
+++ js/script.js
@@ -0,0 +1,192 @@
+/*
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+*/
+
+var app = new Vue({
+    el: '#app',
+    data:{
+        conversations:[
+            {
+                nome: 'Michele',
+                contactImg: '../img/avatar_1.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'si si',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'no no',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Certo!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Francesco',
+                contactImg: '../img/avatar_2.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: "Hai portato a spasso l' orso?",
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati lavare la lavastoviglie',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Gianni',
+                contactImg: '../img/avatar_3.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Melissona',
+                contactImg: '../img/avatar_4.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Berlusconi',
+                contactImg: '../img/avatar_5.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Silvia',
+                contactImg: '../img/avatar_6.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Prugna',
+                contactImg: '../img/avatar_7.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+
+            {
+                nome: 'Fragola',
+                contactImg: '../img/avatar_8.jpg',
+                messages: [
+                    {
+                    date: '10/01/2020 15:30:55',
+                    message: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 15:50:00',
+                    message: 'Ricordati di stendere i panni',
+                    status: 'sent'
+                    },
+                    {
+                    date: '10/01/2020 16:15:22',
+                    message: 'Tutto fatto!',
+                    status: 'received'
+                    }
+                ]
+            },
+        ]
+    } 
+})
